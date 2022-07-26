02d95bb991ecb833bf2f6133f036d09f687e6f0a
Denis Junior <denis_jr2001@hotmail.com>
2022-07-26 16:29:34 -0300

    todoTask with js and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import TodoTask from "./components/TodoTask";
 
 function App() {
+  
+  const [task, setTask] = useState("")
+  const [deadline, setDeadline] = useState(0)
+  const [todoList, setTodoList] = useState([])
+
+  const addTask = () => {
+    const newTask = { taskName: task, deadline };
+    console.log(newTask)
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
+  };
+
+  const completeTask = (taskNameToDelete) => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+
+  const handleChange = (event) => {
+    if (event.target.name === "task") {
+      console.log(event.target.name + "\n value:" + event.target.value)
+      setTask(event.target.value);
+    } else {
+      console.log(event.target.name + "\n value:" + event.target.value)
+      setDeadline(Number(event.target.value));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header task={task} setTask={setTask} deadline={deadline} addTask={addTask} handleChange={handleChange}/>
+      <TodoTask todoList={todoList} completeTask={completeTask}/>
     </div>
   );
 }
